d560e22c6b1fa8b8321311d339978c278b81c5e8
Circlq <103474799+Circlq2@users.noreply.github.com>
2022-08-18 09:58:23 +1200

    Add files via upload

---
+++ Lesson1/test.py
@@ -0,0 +1,13 @@
+# declaration of varibles
+
+base = 5.4
+height = 3
+
+# The statement to calculate the area (using varibles)
+
+(base * height)/2
+
+# Print the result to screen
+
+Print(base * height)/2
+
