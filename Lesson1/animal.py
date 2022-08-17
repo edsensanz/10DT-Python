6ad07d3755efcb52a01754ec8627c93cc883e0a0
Circlq <103474799+Circlq2@users.noreply.github.com>
2022-08-18 09:56:59 +1200

    Add files via upload

---
+++ Lesson1/animal.py
@@ -0,0 +1,13 @@
+# Varibles
+Name='lion'
+numLegs=4
+hasFur=True
+height=1.2
+
+# prepare print statement
+print(type(Name))
+print(type(numLegs))
+print(type(hasFur))
+print(type(height))
+
+# Constuct a readable sentence
