44376f7e9f63003f7dae6998f4d6813cd6bdc838
Circlq <103474799+Circlq2@users.noreply.github.com>
2022-08-23 15:11:15 +1200

    Add files via upload

---
+++ Lesson1/basic_var_output.py
@@ -0,0 +1,14 @@
+# E G Wisnewski
+# Varibles
+
+# Declaration of Varibles
+Name = 'lion'
+numLegs = 4
+hasFur = True
+height = 1.2
+
+# Output to screen
+print(Name)
+print(numLegs)
+print(hasFur)
+print(height)
