82333b72232d0e741a7e633d3e30262b84ddb00a
Circlq <103474799+Circlq2@users.noreply.github.com>
2022-08-25 09:49:01 +1200

    Add files via upload

---
+++ Lesson1/basic_var_output_labels.py
@@ -0,0 +1,20 @@
+# E G Wisnewski
+# Varibles, output, labels
+
+# Declaration of Varibles
+name = 'elephant'
+numLegs = 4
+height = 4.5
+hasTail = True
+
+# Output of varibles to screen
+print(name)
+print(numLegs)
+print(height)
+print(hasTail)
+
+# Output of varibles with labels to screen
+print("The",name,"is one of the big five animals in Africa.")
+print("An",name,"has",numLegs,"legs.")
+print("Its height is approimately",height,"meters.")
+print("And, yes, it's",hasTail,". ", "It has a tail.")
