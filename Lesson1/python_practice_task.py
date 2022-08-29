1146ec9144b91fca95348712ff99cd817fcda4c8
Circlq <103474799+Circlq2@users.noreply.github.com>
2022-08-29 13:36:26 +1200

    Add files via upload

---
+++ Lesson1/python_practice_task.py
@@ -0,0 +1,38 @@
+# E G Wisnewski
+# Varibles, output, labels, data types
+
+# Varibles
+name = "E G Wisnewski"
+height = 1.80
+address = "170 Coutts Street"
+phoneNum = "346565465"
+hasAllergy = True
+
+# A summary of this information should be printe out to the screen
+# output = print()
+print(name)
+print(height)
+print(address)
+print(phoneNum)
+print(hasAllergy)
+
+# Clarify the problem
+# name (var)
+fullName = "Edward Wisnewski"
+firstName = "Edward"
+lastName = "Wisnewski"
+
+# height - expressed in metres
+height = 1.80
+
+# address
+# street number, street name, suburb, city
+address = "170 Coutts Street"
+addNum = "170"
+addStreet = "Coutts Street"
+addSuburb = "Rongotai"
+addCity = "Wellington"
+
+
+# phone number - landline or mobile
+# stored as a string - no calc
