d860dbae0f934075381c214468282c74f64da5f7
Circlq <103474799+Circlq2@users.noreply.github.com>
2022-09-01 10:02:58 +1200

    Add files via upload

---
+++ Lesson1/input_from_user.py
@@ -0,0 +1,38 @@
+# E G Wisnewski
+# User input
+
+# Taking input from the user as interger
+# num = int(input("Enter a number:"))
+# add = num + 1
+
+# output
+# print(add)
+
+# Taking input from the user as float
+# num = float(input("Enter a number:"))
+# add = num + 1
+
+# output
+# print(add)
+
+# Taking input from the user as list
+# li = list(input("Enter a number of 5 digits:"))
+# print(li)
+
+# Taking input from the user as tuple
+num = tuple(input("Enter a number:"))
+print(num)
+
+
+
+#name = input("Enter your first name: ")
+
+#print("Hello,", name,", I hope you have a good day.")
+
+# data type for input
+
+#print("How tall are you?")
+#height = input("Enter your height in Meters: ")
+#print("hello",name ,"it seems that you are",height ,"meters tall")
+#print("What is your favorite colour?")
+#favColour = input("Enter your favorite colour: ")
